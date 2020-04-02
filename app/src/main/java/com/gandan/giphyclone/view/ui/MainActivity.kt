42f2569be8dedd6db295d35f0a5f61ec2d79aca9
pleasantlife <pleasantlife@gmail.com>
2020-04-03 02:14:04 +0900

    200403 ENH : Bottom Navigation view controller update

--- app/src/main/java/com/gandan/giphyclone/view/ui/MainActivity.kt
+++ app/src/main/java/com/gandan/giphyclone/view/ui/MainActivity.kt
@@ -2,26 +2,17 @@ package com.gandan.giphyclone.view.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.gandan.giphyclone.R
-import com.gandan.giphyclone.util.ImageURLListener
-import com.gandan.giphyclone.util.ItemClickListener
-import com.gandan.giphyclone.view.adapter.TrendingAdapter
-import com.gandan.giphyclone.view.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(){
-
-    //private val compositeDisposable = CompositeDisposable()
-    private lateinit var mainViewModel: MainViewModel
-    private lateinit var trendingAdapter : TrendingAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
+        NavigationUI.setupWithNavController(bottomNavigation, findNavController(R.id.nav_host))
     }
 }
